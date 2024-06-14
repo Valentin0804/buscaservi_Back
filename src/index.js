a513ccd13c2e77e1d2e9c69944c79e70eaf8614f
Valentin0804 <77337208+Valentin0804@users.noreply.github.com>
2024-06-14 11:25:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,41 @@ mongoose.connect(dbConfig.url, dbConfig.options)
     .then(() => console.log('Conectado a la base de datos'))
     .catch(err => console.error(err));
 
+// Create
+app.post('/entities', async (req, res) => {
+  try {
+    const newEntity = new Entity(req.body);
+    await newEntity.save();
+    res.status(201).json(newEntity);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete
+app.delete('/entities/:id', getEntity, async (req, res) => {
+  try {
+    await res.entity.remove();
+    res.json({ message: 'Entity deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getEntity(req, res, next) {
+  try {
+    const entity = await Entity.findById(req.params.id);
+    if (entity == null) {
+      return res.status(404).json({ message: 'Entity not found' });
+    }
+    res.entity = entity;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
+
 // Rutas
 const entityRoutes = require('./routes/entityRoutes');
 app.use('/api/entities', entityRoutes);
