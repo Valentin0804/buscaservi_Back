48ed72f33084d13b8d0c6cbd9e4eda2dc270a790
Valentin0804 <valenmoli_18@hotmail.com>
2024-07-25 23:43:57 -0300

    Cambios en databse.js y index.js para comprobar la conexion a base de datos mediante try

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,40 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+// Validar las variables de entorno
+const requiredEnvVars = ['DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME'];
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    console.error(`Error: La variable de entorno ${envVar} no está definida.`);
+    process.exit(1);
+  }
 });
 
-module.exports = connection;
+// Crear el pool de conexiones
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Función para obtener una conexión
+const getConnection = async () => {
+  try {
+    const conn = await connection.getConnection();
+    console.log('Conexión a la base de datos exitosa.');
+    return conn;
+  } catch (error) {
+    console.error('Error conectando a la base de datos:', error);
+    throw error;
+  }
+};
+
+// Exportar el pool de conexiones y la función de conexión
+module.exports = {
+  connection,
+  getConnection
+};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const connection = require('./config/database')
+const { getConnection } = require('./config/database');
+
 
 // Middleware y rutas
 app.use(express.json());
 app.use('/api/users', require('./routes/entityRoutes'));
+
+(async () => {
+  try {
+    await getConnection();
+    console.log('Conexión a la base de datos exitosa.');
+  } catch (error) {
+    console.error('Error conectando a la base de datos:', error);
+    process.exit(1); // Salir del proceso si no se puede conectar a la base de datos
+  }
+})();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
